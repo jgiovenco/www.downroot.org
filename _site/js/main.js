d2770f51d3c45c91ce7b7421881e99c73bbf429d
jgiovenco <39679191+jgiovenco@users.noreply.github.com>
2018-06-17 06:12:58 -0400

    contact form complete

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,28 @@
-console.log('hello');
+//change selection function
+function changeSelection(event) {
+
+  //Prevent default behaviour
+  event.preventDefault();
+
+  //Log
+  console.log('clicked!');
+
+  //Remove all active buttons
+  $('.contactform-section-form-button').removeClass('active');
+
+  //find the clicked button
+  var button = $(event.currentTarget),
+      button_event = button.attr('data-event');
+
+
+  //Set the image to active
+  button.addClass('active');
+
+  //Uncheck all the radio buttons
+  $('.contactform-section-form-radiobuttons input[type="radio"]').prop('checked', false);
+
+  //Check the right radio buttons
+  $('.contactform-section-form-radiobuttons input[value="' + button_event + '"]').prop('checked',true);
+}
+
+$('body').on('click', '.contactform-section-form-button', changeSelection);
