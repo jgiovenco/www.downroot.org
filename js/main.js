d46434937ea6b5f005e27c4c9337c364dde15e82
jgiovenco <39679191+jgiovenco@users.noreply.github.com>
2018-06-17 08:52:10 -0400

    AWESOME PLEDGE PAGE DONE DUDE

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
-//change selection function
+/*
+** Change Selection
+*/
 function changeSelection(event) {
 
-  //Prevent default behaviour
+  // Prevent default behaviour
   event.preventDefault();
 
-  //Log
-  console.log('clicked!');
-
-  //Remove all active buttons
+  // Remove all active buttons
   $('.contactform-section-form-button').removeClass('active');
 
-  //find the clicked button
+  // Find the clicked button
   var button = $(event.currentTarget),
       button_event = button.attr('data-event');
 
-
-  //Set the image to active
+  // Set the button to active
   button.addClass('active');
 
-  //Uncheck all the radio buttons
-  $('.contactform-section-form-radiobuttons input[type="radio"]').prop('checked', false);
+  // Uncheck all the radio buttons
+  $('input[type="radio"]').prop('checked', false);
 
-  //Check the right radio buttons
-  $('.contactform-section-form-radiobuttons input[value="' + button_event + '"]').prop('checked',true);
+  // Check the right radio buttons
+  $('input[value="' + button_event + '"]').prop('checked', true);
 }
 
+/*
+** Scroll to element
+*/
+function scrollToLetter(event) {
+
+  // Prevent following links
+  event.preventDefault();
+
+  // Get clicked navigation letter
+  var letter = $(event.currentTarget),
+      target = letter.attr('data-letter');
+
+  // Scroll to letter H3
+  $('html, body').animate({
+      scrollTop: $('#letter-' + target).offset().top
+  }, 1000);
+
+  // Don't do anything after scrolling
+  return false;
+
+}
+
+// Bind events
+$('body').on('click', '.pledge-archive-navigation a', scrollToLetter);
 $('body').on('click', '.contactform-section-form-button', changeSelection);
